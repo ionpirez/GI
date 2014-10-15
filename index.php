1c2b04d6f8c43bf78f58fe45240ba3273e0ac8b9
oliverpirez <oliver.garcia@ranchodigital.com.mx>
2014-10-15 16:59:56 -0500

    contacto y edificio (fierros)

---
+++ index.php
@@ -0,0 +1,419 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div id="menu">
+<div class="logo"></div>
+<div class="holder">
+	<!--<a data-menuanchor="page1" href="#page1">Inicio</a>
+	<a data-menuanchor="page1" href="#page2">Kajile</a>
+	<a data-menuanchor="page1" href="#page3">Departamentos</a>
+	<a data-menuanchor="page1" href="#page4">Contacto</a>-->
+</div>
+</div>
+	<div id="pagepiling">
+	    <div class="section" id="section1">
+	    	   <div class="texto">
+		    	<!-- <h1>kajile zibatá</h1>
+		    	    <br>
+					<p>vive la experiencia</p>-->
+	    	    </div>
+			<a href="#page2"><span class="box">
+			</span></a>
+		    <div id="slider">
+
+		    </div>
+	    </div>
+	    <div class="section" id="section2">
+	    <div class="text">
+	    	<h1>bla bla bla bla bla bla bla</h1>
+	    	<span>II</span>
+	    	<p>bla bla bla bla bla bla bla bla</p>
+	    </div>
+	    	<div id="gallery">
+	    		<div class="pic" style="background-image:url('css/img/1.jpg');">
+					<div class="caption" style="display: none;">
+						<p>texto texto texto texto</p>
+					</div>
+	    		</div>
+		    	<div class="pic" style="background-image:url('css/img/2.jpg');">
+		    		<div class="caption" style="display: none;">
+						<p>texto texto texto texto</p>
+					</div>
+		    	</div>
+	    		<div class="pic" style="background-image:url('css/img/3.jpg');">
+		    		<div class="caption" style="display: none;">
+						<p>texto texto texto texto</p>
+					</div>
+	    		</div>
+	    		<div class="pic" style="background-image:url('css/img/4.jpg');">
+		    		<div class="caption" style="display: none;">
+						<p>texto texto texto texto</p>
+					</div>
+	    		</div>
+	    		<div class="pic" style="background-image:url('css/img/5.jpg');">
+		    		<div class="caption" style="display: none;">
+						<p>texto texto texto texto</p>
+					</div>
+	    		</div>
+		    	<div class="pic" style="background-image:url('css/img/6.jpg');">
+		    		<div class="caption" style="display: none;">
+						<p>texto texto texto texto</p>
+					</div>
+		    	</div>
+	    		<div class="pic" style="background-image:url('css/img/7.jpg');">
+		    		<div class="caption" style="display: none;">
+						<p>texto texto texto texto</p>
+					</div>
+	    		</div>
+    			<div class="pic" style="background-image:url('css/img/8.jpg');">
+		    		<div class="caption" style="display: none;">
+						<p>texto texto texto texto</p>
+					</div>	
+	    		</div>
+	    	</div>
+	    </div>
+	    <div class="section" id="section3">
+	    	<!--edificio-->
+			<div id="building">
+				<div id="inner">
+					<div class="full view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="half view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="half view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="half view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="medium view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="half view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="quarter view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="medium view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="medium view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="full view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="half view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="half view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="half view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+					<div class="full view view-tenth" style="background-image:url('css/img/1.jpg');">
+		                    <div class="mask">
+		                        <a class="info" href="css/img/1.jpg" data-lightbox="depa-1"> 
+		                        Más informacion</a>
+		                        <a class="info" href="css/img/2.jpg" data-lightbox="depa-1" style="display:none;"></a>
+    	                        <a class="info" href="css/img/3.jpg" data-lightbox="depa-1" style="display:none;"></a>
+		                    </div>
+					</div>
+				</div>
+			</div>
+			<!--edificio-->
+	    </div>
+	    <div class="section" id="section4">
+
+<div id="contact">
+		<div class="container content">
+		
+	        <?php if(isset($emailSent) && $emailSent == true) { ?>
+                <p class="info">Your email was sent. Huzzah!</p>
+            <?php } else { ?>
+            
+				<div class="desc">
+					<h2>Contactanos</h2>
+					<p>ubicacion</p> <br>
+					<p>direccion</p>
+				</div>
+				<div class="contact">
+					<?php if(isset($hasError) || isset($captchaError) ) { ?>
+                        <p class="alert">Error submitting the form</p>
+                    <?php } ?>
+				
+					<form id="contact-us" action="contact.php" method="post">
+						<div class="formblock">
+							<label class="screen-reader-text">Name</label>
+							<input type="text" name="contactName" id="contactName" value="<?php if(isset($_POST['contactName'])) echo $_POST['contactName'];?>" class="txt requiredField" placeholder="Name:" />
+							<?php if($nameError != '') { ?>
+								<br /><span class="error"><?php echo $nameError;?></span> 
+							<?php } ?>
+						</div>
+                        
+						<div class="formblock">
+							<label class="screen-reader-text">Email</label>
+							<input type="text" name="email" id="email" value="<?php if(isset($_POST['email']))  echo $_POST['email'];?>" class="txt requiredField email" placeholder="Email:" />
+							<?php if($emailError != '') { ?>
+								<br /><span class="error"><?php echo $emailError;?></span>
+							<?php } ?>
+						</div>
+                        
+						<div class="formblock">
+							<label class="screen-reader-text">Message</label>
+							 <textarea name="comments" id="commentsText" class="txtarea requiredField" placeholder="Message:"><?php if(isset($_POST['comments'])) { if(function_exists('stripslashes')) { echo stripslashes($_POST['comments']); } else { echo $_POST['comments']; } } ?></textarea>
+							<?php if($commentError != '') { ?>
+								<br /><span class="error"><?php echo $commentError;?></span> 
+							<?php } ?>
+						</div>
+                        
+							<button name="submit" type="submit" class="subbutton">Send us Mail!</button>
+							<input type="hidden" name="submitted" id="submitted" value="true" />
+					</form>			
+				</div>
+				
+			<?php } ?>
+		</div>
+    </div><!-- End #contact -->	    </div>
+	</div>
+</body>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="js/jquery.pagepiling.js"></script>
+	<script src="js/jquery.hoverGrid.js"></script>
+	<script src="js/lightbox.js"></script>
+		<script type="text/javascript">
+		$(document).ready(function() {
+
+			/*
+			* Plugin intialization
+			*/
+	    	$('#pagepiling').pagepiling({
+	    		menu: '#menu',
+	    		anchors: ['page1', 'page2', 'page3', 'page4'],
+			    sectionsColor: ['#fff', '#ECECEC', '#ECECEC', '#ECECEC'],
+			    navigation: {
+			    	'position': 'right',
+			   		'tooltips': ['Inico', 'Kajile', 'Inmuebles', 'Contacto']
+			   	},
+			    afterRender: function(){
+			    	$('#pp-nav').addClass('custom');
+			    },
+			    afterLoad: function(anchorLink, index){
+			    	if(index>1){
+			    		$('#pp-nav').removeClass('custom');
+			    	}else{
+			    		$('#pp-nav').addClass('custom');
+			    	}
+			    }
+			});
+
+
+			/*
+		    * Internal use of the demo website
+		    */
+		    $('#showExamples').click(function(e){
+				e.stopPropagation();
+				e.preventDefault();
+				$('#examplesList').toggle();
+			});
+
+			$('html').click(function(){
+				$('#examplesList').hide();
+			});
+	    });
+    </script>
+    <script>
+jQuery(window).load(function(){
+    var images = ['css/img/1.jpg','css/img/2.jpg', 'css/img/3.jpg', 'css/img/4.jpg', 'css/img/5.jpg', 'css/img/6.jpg', 'css/img/7.jpg', 'css/img/8.jpg', 'css/img/9.jpg', 'css/img/10.jpg', 'css/img/11.jpg' , 'css/img/12.jpg', 'css/img/13.jpg', 'css/img/14.jpg'];
+    var i = 0;
+    var timeoutVar;
+
+    function changeBackground() {
+        clearTimeout(timeoutVar);
+        $('#slider').fadeOut('slow', function() {
+			jQuery('#slider').css('background-image', function() {
+				if (i >= images.length) {
+				    i=0;
+			}
+			return 'url(' + images[i++] + ')';      
+			});
+        });
+        $('#slider').fadeIn('slow');
+        timeoutVar = setTimeout(changeBackground, 7000);
+    }
+    changeBackground();        
+});
+    </script>
+
+    <script type="text/javascript">
+    $(document).ready(function() {
+      $('#gallery').hoverGrid();
+    });
+</script>
+<script type="text/javascript">
+	<!--//--><![CDATA[//><!--
+	$(document).ready(function() {
+		$('form#contact-us').submit(function() {
+			$('form#contact-us .error').remove();
+			var hasError = false;
+			$('.requiredField').each(function() {
+				if($.trim($(this).val()) == '') {
+					var labelText = $(this).prev('label').text();
+					$(this).parent().append('<span class="error">Your forgot to enter your '+labelText+'.</span>');
+					$(this).addClass('inputError');
+					hasError = true;
+				} else if($(this).hasClass('email')) {
+					var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+					if(!emailReg.test($.trim($(this).val()))) {
+						var labelText = $(this).prev('label').text();
+						$(this).parent().append('<span class="error">Sorry! You\'ve entered an invalid '+labelText+'.</span>');
+						$(this).addClass('inputError');
+						hasError = true;
+					}
+				}
+			});
+			if(!hasError) {
+				var formInput = $(this).serialize();
+				$.post($(this).attr('action'),formInput, function(data){
+					$('form#contact-us').slideUp("fast", function() {				   
+						$(this).before('<p class="tick"><strong>Thanks!</strong> Your email has been delivered. Huzzah!</p>');
+					});
+				});
+			}
+			
+			return false;	
+		});
+	});
+	//-->!]]>
+</script>
+
+<?php 
+error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
+
+//If the form is submitted
+if(isset($_POST['submitted'])) {
+	
+	// require a name from user
+	if(trim($_POST['contactName']) === '') {
+		$nameError =  'Forgot your name!'; 
+		$hasError = true;
+	} else {
+		$name = trim($_POST['contactName']);
+	}
+	
+	// need valid email
+	if(trim($_POST['email']) === '')  {
+		$emailError = 'Forgot to enter in your e-mail address.';
+		$hasError = true;
+	} else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($_POST['email']))) {
+		$emailError = 'You entered an invalid email address.';
+		$hasError = true;
+	} else {
+		$email = trim($_POST['email']);
+	}
+		
+	// we need at least some content
+	if(trim($_POST['comments']) === '') {
+		$commentError = 'You forgot to enter a message!';
+		$hasError = true;
+	} else {
+		if(function_exists('stripslashes')) {
+			$comments = stripslashes(trim($_POST['comments']));
+		} else {
+			$comments = trim($_POST['comments']);
+		}
+	}
+		
+	// upon no failure errors let's email now!
+	if(!isset($hasError)) {
+		
+		$emailTo = 'olipirez@gmail.com';
+		$subject = 'mensaje enviado de: '.$name;
+		$sendCopy = trim($_POST['sendCopy']);
+		$body = "Name: $name \n\nEmail: $email \n\nComments: $comments";
+		$headers = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+
+		mail($emailTo, $subject, $body, $headers);
+        
+        // set our boolean completion value to TRUE
+		$emailSent = true;
+	}
+}
+?>
+</html>
